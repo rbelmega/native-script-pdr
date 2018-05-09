9266b97de66e5f6028270426a2a1e389caed2dad
rbelmeha <belmega31@gmail.com>
2018-05-09 11:30:24 +0300

    list-item

--- app/components/sign/sign.component.ts
+++ app/components/sign/sign.component.ts
@@ -5,56 +5,10 @@ import 'rxjs/add/operator/switchMap';
 import { DataService } from '../../services/data.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'pdr-topic',
-  styles: [
-    `
-.panel {
-	background-color: #f2f2f2;
-	color: rgb(66, 94, 106);
-	font-size: 14px;
-	line-height: 20px;
-	text-shadow: 0px 1px rgb(255, 255, 255);
-}
-
-.list-item {
-    margin-top: 20px;
-	margin-bottom: 20px;
-	border-radius: 2px;
-	padding-top: 20px;
-	padding-bottom: 20px;
-	padding-right: 15px;
-	padding-left: 15px;
-	color: #000000;
-	background: #ffffff;
-	border: 0;
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-	border-bottom-color: rgb(204, 204, 204);
-	box-shadow: 0 4px 4px #e2e2e2;
-	width: 100%;
-}
-.sign-image {
-    display: inline-block;
-    width: 50px;
-    margin-right: 30px;
-}
-  `,
-  ],
-  template: `
-<ScrollView orientation="vertical" class="panel">
-    <WrapLayout >
-        <WrapLayout *ngFor="let list of topic; let i = index">
-            <WrapLayout class="list-item">
-               <WrapLayout *ngFor="let hh of list.images">
-                   <Image class="sign-image" src="{{ '~/assets/images/sign/' + signCategory + '/' + hh.src }}" class="sign-image"></Image>
-                   <Label text="{{ hh.desc }}"></Label>
-               </WrapLayout>
-               <HtmlView [html]="list.desc"></HtmlView>
-               </WrapLayout>
-        </WrapLayout>
-    </WrapLayout>
-</ScrollView>
-`,
+  styleUrls: ['sign.component.scss'],
+  templateUrl: 'sign.component.html',
 })
 export class SignComponent implements OnInit {
   public sub: any;
--- app/components/topic/topic.component.ts
+++ app/components/topic/topic.component.ts
@@ -5,62 +5,10 @@ import 'rxjs/add/operator/switchMap';
 import { DataService } from '../../services/data.service';
 
 @Component({
+  moduleId: module.id,
   selector: 'pdr-topic',
-  styles: [
-    `
-.panel {
-	background-color: #f2f2f2;
-	color: rgb(66, 94, 106);
-	font-size: 14px;
-	line-height: 20px;
-	text-shadow: 0px 1px rgb(255, 255, 255);
-}
-
-.list-item {
-    margin-top: 20px;
-	margin-bottom: 20px;
-	border-radius: 2px;
-	padding-top: 20px;
-	padding-bottom: 20px;
-	padding-right: 15px;
-	padding-left: 15px;
-	color: #000000;
-	background: #ffffff;
-	border: 0;
-	border-bottom-width: 1px;
-	border-bottom-style: solid;
-	border-bottom-color: rgb(204, 204, 204);
-	box-shadow: 0 4px 4px #e2e2e2;
-	width: 100%;
-}
-  `,
-  ],
-  template: `
-<ScrollView orientation="vertical" class="panel">
-    <StackLayout>
-        <StackLayout *ngFor="let list of topic.nodes; let i = index">
-           <Label
-              *ngIf="list.type == 'p'"
-              class="list-item" 
-              text="{{list.value}}"
-              textWrap="true"
-            >
-            </Label>
-            <StackLayout *ngIf="list.type == 'list'">
-                <StackLayout *ngFor="let li of list.value">
-                    <Label
-                      *ngIf="li.type == 'list-item'"
-                      class="list-item" 
-                      text="{{li.value}}"
-                      textWrap="true"
-                    >
-                    </Label>
-                </StackLayout>
-             </StackLayout>
-        </StackLayout>
-    </StackLayout>
-</ScrollView>
-`,
+  styleUrls: ['topic.component.scss'],
+  templateUrl: 'topic.component.html',
 })
 export class TopicComponent implements OnInit {
   public sub: any;
